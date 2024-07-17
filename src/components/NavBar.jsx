d6e590bec68292499d9a0c3851975e3e9708bbf6
Guilherme.lima <Guilherme.lima@teigs.com.br>
2024-07-17 10:19:02 -0300

    added hover on icons in navBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,34 +4,34 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdGTranslate } from "react-icons/md";
 import { MdDownload } from "react-icons/md";
 
-
 const NavBar = ({ translate, setTranslate }) => {
   return (
     <>
-      <nav className="mb-20 flex items-center justify-between py-6">
+      <nav className="mb-20 flex items-center justify-between py-2">
         <div className="flex flex-shrink-0 items-center">
           <img className="mx-2 w-10" src={logo} alt="Logo" />
         </div>
-        <div className="flex m-8 items-center justify-center gap-4 text-2xl cursor-pointer">
+        <div className="flex m-8 items-center justify-center gap-4 text-2xl">
           <MdGTranslate
+            className="hover:text-orange-500 transition duration-300 cursor-pointer"
             onClick={() => {
               setTranslate(!translate);
             }}
           />
           <FaLinkedin
+            className="hover:text-orange-500 transition duration-300 cursor-pointer"
             onClick={() => {
-              window.open(
-                "https://www.linkedin.com/in/guilherme-lopes-lima-323324219/"
-              );
+              window.open("https://www.linkedin.com/in/guilherme-lopes-lima-323324219/");
             }}
-          /> 
+          />
           <FaGithub
+            className="hover:text-orange-500 transition duration-300 cursor-pointer"
             onClick={() => {
               window.open("https://github.com/guilhermelpl");
             }}
           />
           <a href={resume} download="GuilhermeLopesLima.pdf">
-            <MdDownload className="mt-1" />
+            <MdDownload className="hover:text-orange-500 transition duration-300 cursor-pointer mt-1" />
           </a>
         </div>
       </nav>
